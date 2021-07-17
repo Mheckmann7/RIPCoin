6ddf6bdfcbba4f2926743c00c265e365b168c955
Mira Heckmann <miraheckmann@gmail.com>
2021-07-16 20:28:52 -0400

    final tokenization

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,36 @@
 const path = require("path");
+require("dotenv").config({ path: "./.env" });
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
+
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
+    development: {
       port: 7545,
       network_id: "*",
       host: "127.0.0.1"
+    },
+    ganache_local: {
+      provider: function () {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", AccountIndex)
+      },
+      network_id: 1337
+    },
+    // ropsten_infura: {
+    //   provider: function() {
+    //     return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/72eb22f5e2784d8c985a2f7eb3b65b12", AccountIndex)
+    //   },
+    //   network_id: 3
+    // },
+    goerli_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/72eb22f5e2784d8c985a2f7eb3b65b12", AccountIndex)
+      },
+      network_id: 5
     }
   },
   compilers: {    
